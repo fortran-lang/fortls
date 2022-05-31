c0c0dfbf41ccd785ac74264cdff5ca7bff0b0543
gnikit <giannis.nikiteas@gmail.com>
2022-05-31 07:34:10 +0100

    Add unittests for error posting

---
+++ test/test_server_messages.py
@@ -0,0 +1,30 @@
+from setup_tests import run_request, test_dir, write_rpc_request
+
+
+def check_msg(ref, res):
+    assert ref["type"] == res["type"]
+    assert ref["message"] == res["message"]
+
+
+# def test_config_file_non_existent():
+#     string = write_rpc_request(1, "initialize", {"rootPath": str(test_dir)})
+#     errcode, results = run_request(string, ["-c", "fake.json"])
+#
+#     ref = {"type": 1, "message": "Configuration file 'fake.json' not found"}
+#     assert errcode == 0
+#     check_msg(ref, results[0])
+
+
+def test_config_file_non_existent_options():
+    string = write_rpc_request(1, "initialize", {"rootPath": str(test_dir)})
+    errcode, results = run_request(string, ["-c", "wrong_syntax.json"])
+
+    ref = {
+        "type": 1,
+        "message": (
+            "Error: 'Expecting ':' delimiter: line 2 column 18 (char 19)' while reading"
+            " 'wrong_syntax.json' Configuration file"
+        ),
+    }
+    assert errcode == 0
+    check_msg(ref, results[0])
