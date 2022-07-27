4e3c95aa6425190c8dc59c66860a89e74c7d1992
Julian Andrej <andrej1@llnl.gov>
2022-07-27 08:21:43 -0700

    add usedforsecurity flag to md5 hashing

--- fortls/parse_fortran.py
+++ fortls/parse_fortran.py
@@ -872,7 +872,7 @@ class FortranFile:
             return "Could not read/decode file", None
         else:
             # Check if files are the same
-            hash = hashlib.md5(contents.encode("utf-8")).hexdigest()
+            hash = hashlib.md5(contents.encode("utf-8"), usedforsecurity=False).hexdigest()
             if hash == self.hash:
                 return None, False
 
