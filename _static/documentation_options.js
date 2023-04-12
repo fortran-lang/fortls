16371c11877cdc2e526e517ec49ab9b7d4e3c215
gnikit <gnikit@users.noreply.github.com>
2023-04-12 08:55:54 +0000

    deploy: 5d75ce8247307c9581f83e9152d8bf6e0a11f408

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+ge4866b7',
+    VERSION: '0.1.dev1+g5d75ce8',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
