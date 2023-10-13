6703214f3698b35d173d290fb77777916a74cfe0
Aljen Uitbeijerse <aljen.uitbeijerse@vortech.nl>
2023-10-13 21:17:50 +0200

    test: Update test for preprocessor #elif parsing
    Only content from the true branch should be in the final parsed file

--- test/test_source/pp/preproc_elif.F90
+++ test/test_source/pp/preproc_elif.F90
@@ -2,10 +2,15 @@ subroutine preprocessor_elif(var)
 
 #if 0
 #define MYTYPE character
+integer, intent(in) :: var
 #elif 1
 #define MYTYPE logical
+integer, intent(inout) :: var
+var = 3
 #else
 #define MYTYPE integer
+integer, intent(out) :: var
+var = 5
 #endif
 
 MYTYPE :: var1
@@ -13,5 +18,7 @@ MYTYPE :: var1
 #define OTHERTYPE integer
 
 OTHERTYPE :: var2
+
+PRINT*, var
 
 endsubroutine preprocessor_elif
