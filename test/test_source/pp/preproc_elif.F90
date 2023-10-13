d18b2eaafbd430def28fe394bf17df4c460a2fbb
Aljen Uitbeijerse <aljen.uitbeijerse@vortech.nl>
2023-10-13 23:24:36 +0200

    test: update test for preprocessor #elif parsing
    Only the first true branch of a #if-#elif-#else should be included

--- test/test_source/pp/preproc_elif.F90
+++ test/test_source/pp/preproc_elif.F90
@@ -1,4 +1,4 @@
-subroutine preprocessor_elif(var)
+subroutine preprocessor_elif(var, var3, var4, var5, var6)
 
 #if 0
 #define MYTYPE character
@@ -20,5 +20,120 @@ MYTYPE :: var1
 OTHERTYPE :: var2
 
 PRINT*, var
+
+#if 1
+#define PART1 1,
+#elif 0
+#define PART2 2,
+#elif 1
+#define PART3 3,
+#else
+#define PART4 4
+#endif
+
+#ifndef PART1
+#define PART1 5,
+#endif
+#ifndef PART2
+#define PART2 5,
+#endif
+#ifndef PART3
+#define PART3 5,
+#endif
+#ifndef PART4
+#define PART4 5
+#endif
+
+REAL(PART1 PART2 PART3 PART4) :: var3
+
+#undef PART1
+#undef PART2
+#undef PART3
+#undef PART4
+
+#if 0
+#define PART1 1,
+#elif 1
+#define PART2 2,
+#elif 1
+#define PART3 3,
+#else
+#define PART4 4
+#endif
+
+#ifndef PART1
+#define PART1 5,
+#endif
+#ifndef PART2
+#define PART2 5,
+#endif
+#ifndef PART3
+#define PART3 5,
+#endif
+#ifndef PART4
+#define PART4 5
+#endif
+
+REAL(PART1 PART2 PART3 PART4) :: var4
+
+#undef PART1
+#undef PART2
+#undef PART3
+#undef PART4
+
+#if 0
+#define PART1 1,
+#elif 0
+#define PART2 2,
+#elif 0
+#define PART3 3,
+#else
+#define PART4 4
+#endif
+
+#ifndef PART1
+#define PART1 5,
+#endif
+#ifndef PART2
+#define PART2 5,
+#endif
+#ifndef PART3
+#define PART3 5,
+#endif
+#ifndef PART4
+#define PART4 5
+#endif
+
+REAL(PART1 PART2 PART3 PART4) :: var5
+
+#undef PART1
+#undef PART2
+#undef PART3
+#undef PART4
+
+#if 1
+#define PART1 1,
+#elif 1
+#define PART2 2,
+#elif 0
+#define PART3 3,
+#else
+#define PART4 4
+#endif
+
+#ifndef PART1
+#define PART1 5,
+#endif
+#ifndef PART2
+#define PART2 5,
+#endif
+#ifndef PART3
+#define PART3 5,
+#endif
+#ifndef PART4
+#define PART4 5
+#endif
+
+REAL(PART1 PART2 PART3 PART4) :: var6
 
 endsubroutine preprocessor_elif
