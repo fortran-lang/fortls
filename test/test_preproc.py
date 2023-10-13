a1645e67b74c7860126eb481e2479e67b69f9ba3
Aljen Uitbeijerse <aljen.uitbeijerse@vortech.nl>
2023-10-13 23:25:31 +0200

    fix: preprocessor #elif only includes first true branch

--- test/test_preproc.py
+++ test/test_preproc.py
@@ -37,6 +37,10 @@ def test_hover():
     string += hover_req(file_path, 15, 12)
     string += hover_req(file_path, 19, 15)
     string += hover_req(file_path, 21, 10)
+    string += hover_req(file_path, 46, 36)
+    string += hover_req(file_path, 76, 36)
+    string += hover_req(file_path, 106, 36)
+    string += hover_req(file_path, 136, 36)
     config = str(root_dir / ".pp_conf.json")
     errcode, results = run_request(string, ["--config", config])
     assert errcode == 0
@@ -61,6 +65,10 @@ def test_hover():
         "```fortran90\nLOGICAL :: var1\n```",
         "```fortran90\nINTEGER :: var2\n```",
         "```fortran90\nINTEGER, INTENT(INOUT) :: var\n```",
+        "```fortran90\nREAL(1, 5, 5, 5) :: var3\n```",
+        "```fortran90\nREAL(5, 2, 5, 5) :: var4\n```",
+        "```fortran90\nREAL(5, 5, 5, 4) :: var5\n```",
+        "```fortran90\nREAL(1, 5, 5, 5) :: var6\n```",
     )
     assert len(ref_results) == len(results) - 1
     check_return(results[1:], ref_results)
