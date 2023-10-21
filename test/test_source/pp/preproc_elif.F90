4f7bc5c760219ac774bbcdd00110093d599b2193
gnikit <giannis.nikiteas@gmail.com>
2023-10-21 15:53:11 +0100

    refactor: preprocessor elif test

--- test/test_source/pp/preproc_elif.F90
+++ test/test_source/pp/preproc_elif.F90
@@ -1,139 +1,27 @@
 subroutine preprocessor_elif(var, var3, var4, var5, var6)
 
+! This file, as used in test_preproc, checks that
+! 1. the steps after the preprocessor parsing has fully finished, are only
+! using content from the parts within the preprocessor if-elif-else that
+! should be used. To do this, it has some regular fortran code within the
+! #if and #elif.
+! 2. the #endif correctly concludes the if-elif, so any new #define statements
+! that come after the #endif, are picked up during the preprocessor parsing.
+
 #if 0
-#define MYTYPE character
 integer, intent(in) :: var
 #elif 1
-#define MYTYPE logical
 integer, intent(inout) :: var
 var = 3
 #else
-#define MYTYPE integer
 integer, intent(out) :: var
 var = 5
 #endif
-
-MYTYPE :: var1
 
 #define OTHERTYPE integer
 
 OTHERTYPE :: var2
 
 PRINT*, var
-
-#if 1
-#define PART1 1,
-#elif 0
-#define PART2 2,
-#elif 1
-#define PART3 3,
-#else
-#define PART4 4
-#endif
-
-#ifndef PART1
-#define PART1 5,
-#endif
-#ifndef PART2
-#define PART2 5,
-#endif
-#ifndef PART3
-#define PART3 5,
-#endif
-#ifndef PART4
-#define PART4 5
-#endif
-
-REAL(PART1 PART2 PART3 PART4) :: var3
-
-#undef PART1
-#undef PART2
-#undef PART3
-#undef PART4
-
-#if 0
-#define PART1 1,
-#elif 1
-#define PART2 2,
-#elif 1
-#define PART3 3,
-#else
-#define PART4 4
-#endif
-
-#ifndef PART1
-#define PART1 5,
-#endif
-#ifndef PART2
-#define PART2 5,
-#endif
-#ifndef PART3
-#define PART3 5,
-#endif
-#ifndef PART4
-#define PART4 5
-#endif
-
-REAL(PART1 PART2 PART3 PART4) :: var4
-
-#undef PART1
-#undef PART2
-#undef PART3
-#undef PART4
-
-#if 0
-#define PART1 1,
-#elif 0
-#define PART2 2,
-#elif 0
-#define PART3 3,
-#else
-#define PART4 4
-#endif
-
-#ifndef PART1
-#define PART1 5,
-#endif
-#ifndef PART2
-#define PART2 5,
-#endif
-#ifndef PART3
-#define PART3 5,
-#endif
-#ifndef PART4
-#define PART4 5
-#endif
-
-REAL(PART1 PART2 PART3 PART4) :: var5
-
-#undef PART1
-#undef PART2
-#undef PART3
-#undef PART4
-
-#if 1
-#define PART1 1,
-#elif 1
-#define PART2 2,
-#elif 0
-#define PART3 3,
-#else
-#define PART4 4
-#endif
-
-#ifndef PART1
-#define PART1 5,
-#endif
-#ifndef PART2
-#define PART2 5,
-#endif
-#ifndef PART3
-#define PART3 5,
-#endif
-#ifndef PART4
-#define PART4 5
-#endif
-
-REAL(PART1 PART2 PART3 PART4) :: var6
 
 endsubroutine preprocessor_elif
---
+++ test/test_source/pp/preproc_elif_elif_skip.F90
@@ -0,0 +1,33 @@
+subroutine preprocessor_elif_elif_skip()
+
+! This file, as used in test_preproc, and together with the two similar files,
+! tests that when there is an if-elif-elif-else, only the first branch that
+! evaluates to true is used, and the others ignored. Also when multiple
+! conditions evaluate to true.
+
+#if 0
+#define PART1 0
+#elif 1
+#define PART2 1
+#elif 1
+#define PART3 0
+#else
+#define PART4 0
+#endif
+
+#ifndef PART1
+#define PART1 0
+#endif
+#ifndef PART2
+#define PART2 0
+#endif
+#ifndef PART3
+#define PART3 0
+#endif
+#ifndef PART4
+#define PART4 0
+#endif
+
+integer, parameter :: res = PART1+PART2+PART3+PART4
+
+end subroutine preprocessor_elif_elif_skip
