4367f3200d026220a1820ecfc59d76fa79dcfd1d
Randy Eckman <randy.eckman@nasa.gov>
2024-01-02 21:29:57 -0600

    everything working but the def regex

--- test/test_source/pp/preproc_spacing_arg_defs.F90
+++ test/test_source/pp/preproc_spacing_arg_defs.F90
@@ -8,32 +8,32 @@ program preprocessor_spacing_arg_defs
       integer, public :: test_int
 
     contains
-      generic, public :: print_test => MAYBEWRAP(test_type_print_test)
-      procedure :: MAYBEWRAP(test_type_print_test)
+      generic, public :: set_test => MAYBEWRAP(test_type_set_test)
+      procedure :: MAYBEWRAP(test_type_set_test)
 
   end type test_type
 
   type(test_type) :: the_test
 
-  call the_test%print_test()
+  call the_test%set_test()
 
   integer, parameter :: argtest = MACROARGS(me%test_int, 4)
 
 contains
-  subroutine test_type_print_test(me)
+  subroutine test_type_set_test(me)
     implicit none
 
     class(test_type), intent(inout) :: me
 
     me%test_int = 3
-  end subroutine json_file_load_from_string
+  end subroutine test_type_set_test
 
-  subroutine wrap_test_type_print_test(me)
+  subroutine wrap_test_type_set_test(me)
     implicit none
 
     class(test_type), intent(inout) :: me
 
     me%test_int = 5
-  end subroutine wrap_test_type_print_test
+  end subroutine wrap_test_type_set_test
 
 end program preprocessor_spacing_arg_defs
