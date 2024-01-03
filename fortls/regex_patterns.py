ccb4d3734a3985438431cbd4d6a4786e4f923623
Randy Eckman <randy.eckman@nasa.gov>
2024-01-03 12:59:12 -0600

    replace \s* with [ ]*

--- fortls/regex_patterns.py
+++ fortls/regex_patterns.py
@@ -124,14 +124,14 @@ class FortranRegularExpressions:
     FREE_FORMAT_TEST: Pattern = compile(r"[ ]{1,4}[a-z]", I)
     # Preprocessor matching rules
     DEFINED: Pattern = compile(r"defined[ ]*\(?[ ]*([a-z_]\w*)[ ]*\)?", I)
-    PP_REGEX: Pattern = compile(r"\s*#\s*(if |ifdef|ifndef|else|elif|endif)")
+    PP_REGEX: Pattern = compile(r"[ ]*#[ ]*(if |ifdef|ifndef|else|elif|endif)")
     PP_DEF: Pattern = compile(
-        r"\s*#\s*(define|undef)\s*(\w+)(\(\s*(\w+\s*,?[\s\w,]*)*\s*\))?",
+        r"[ ]*#[ ]*(define|undef)[ ]*(\w+)(\([ ]*(\w+[ ]*,?[ \w,]*)*[ ]*\))?",
         I,
     )
     PP_DEF_TEST: Pattern = compile(r"(![ ]*)?defined[ ]*\([ ]*(\w*)[ ]*\)$", I)
-    PP_INCLUDE: Pattern = compile(r"\s*#\s*include\s*([\"\w\.]*)", I)
-    PP_ANY: Pattern = compile(r"(^\s*#\s*\w*:?\w+)")
+    PP_INCLUDE: Pattern = compile(r"[ ]*#[ ]*include[ ]*([\"\w\.]*)", I)
+    PP_ANY: Pattern = compile(r"(^[ ]*#[ ]*\w*:?\w+)")
     # Context matching rules
     CALL: Pattern = compile(r"[ ]*CALL[ ]+[\w%]*$", I)
     INT_STMNT: Pattern = compile(r"^[ ]*[a-z]*$", I)
