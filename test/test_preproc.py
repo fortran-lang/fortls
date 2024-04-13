aa18a6819b71c912335e5fa185fcc777da8423ac
gnikit <giannis.nikiteas@gmail.com>
2024-04-13 17:22:50 +0100

    refactor: remove preprocessor whitespace test
    This test is problematic for a few reasons and is in need of refactoring. A summary of the issues are:
    - macro expansions are already covered in another test
    - test too big
    - it pollutes the global workspace of symbols
    - names of objects are too generic
    - tests indirectly the preprocessor
    
    Its purpose in this PR is to test the preprocessor REGEX patterns work
    with leading whitespaces and can perform macro expansions.
    I have opted to remove and write a more compact test.

--- test/test_preproc.py
+++ test/test_preproc.py
@@ -42,10 +42,6 @@ def test_hover():
     string += hover_req(file_path, 30, 23)
     file_path = root_dir / "preproc_if_elif_skip.F90"
     string += hover_req(file_path, 30, 23)
-    file_path = root_dir / "preproc_spacing_arg_defs.F90"
-    string += hover_req(file_path, 11, 20)
-    string += hover_req(file_path, 20, 17)
-    string += hover_req(file_path, 22, 13)
     config = str(root_dir / ".pp_conf.json")
     errcode, results = run_request(string, ["--config", config])
     assert errcode == 0
@@ -72,9 +68,6 @@ def test_hover():
         "```fortran90\nINTEGER, PARAMETER :: res = 0+1+0+0\n```",
         "```fortran90\nINTEGER, PARAMETER :: res = 0+0+0+1\n```",
         "```fortran90\nINTEGER, PARAMETER :: res = 1+0+0+0\n```",
-        "```fortran90\n#define MAYBEWRAP(PROCEDURE) PROCEDURE\n```",
-        "```fortran90\nSUBROUTINE test_type_set_test()\n```",
-        "```fortran90\n#define MACROARGS(x, y) x + y\n```",
     )
     assert len(ref_results) == len(results) - 1
     check_return(results[1:], ref_results)
