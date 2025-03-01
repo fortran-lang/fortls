e363056876951a85ebf48c2d2597a7b1eaba795c
gnikit <giannis.nikiteas@gmail.com>
2025-03-01 10:57:59 +0000

    test: add reference exclusion test for fixed format

---
+++ test/test_source/fixed/comment_as_reference.f
@@ -0,0 +1,5 @@
+      program comment_as_reference
+C     Comment with variable name gets picked as a ref: variable_to_reference
+      real variable_to_reference
+      variable_to_reference = 1
+      end program comment_as_reference
